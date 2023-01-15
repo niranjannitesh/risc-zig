b899d2d1febb49791cdbc1f94f40b2c5822b2948
Nitesh Kumar Niranjan <hello@theux.dev>
2023-01-15 07:25:55 +0000

    feat: csr and A

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const constants = @import("./constants.zig");
 const MemoryMap = @import("./memory-map.zig").MemoryMap;
+const CSR = @import("./csr.zig").CSR;
 const ArrayList = std.ArrayList;
 
 const CPUError = error{InvalidInstructionError};
@@ -11,10 +12,18 @@ const reg_name = [_][]const u8{
     "s6",   "s7", "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6",
 };
 
+const Mode = enum(u64) {
+    Machine = 0b11,
+    Supervisor = 0b01,
+    User = 0b00,
+};
+
 pub const CPU = struct {
     regs: [32]u64,
     pc: u64,
     mm: MemoryMap,
+    csr: CSR,
+    mode: Mode = .Machine,
 
     const Self = @This();
 
@@ -25,6 +34,7 @@ pub const CPU = struct {
             .regs = regs,
             .pc = constants.RAM_BASE_ADDR,
             .mm = try MemoryMap.init(code),
+            .csr = CSR.init(),
         };
     }
 
@@ -33,6 +43,25 @@ pub const CPU = struct {
             std.debug.print("{s:04} = x{:<02}[{x:>18}]\t{s:04} = x{:<02}[{x:>18}]\t{s:04} = x{:<02}[{x:>18}]\t{s:04} = x{:<02}[{x:>18}]\n", .{ reg_name[i], i, self.regs[i], reg_name[i + 1], i + 1, self.regs[i + 1], reg_name[i + 2], i + 2, self.regs[i + 2], reg_name[i + 3], i + 3, self.regs[i + 3] });
             i += 3;
         }
+        std.debug.print("\n\n{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\n{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\n{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\n{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\n{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\n{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\n", .{
+            "pc",         self.pc,
+            "mhartid",    self.csr.load(constants.MHARTID),
+            "mstatus",    self.csr.load(constants.MSTATUS),
+            "mtvec",      self.csr.load(constants.MTVEC),
+            "mepc",       self.csr.load(constants.MEPC),
+            "mcause",     self.csr.load(constants.MCAUSE),
+            "mtval",      self.csr.load(constants.MTVAL),
+            "medeleg",    self.csr.load(constants.MEDELEG),
+            "mscratch",   self.csr.load(constants.MSCRATCH),
+            "MIP",        self.csr.load(constants.MIP),
+            "mcounteren", self.csr.load(constants.MCOUNTEREN),
+            "sstatus",    self.csr.load(constants.SSTATUS),
+            "stvec",      self.csr.load(constants.STVEC),
+            "sepc",       self.csr.load(constants.SEPC),
+            "scause",     self.csr.load(constants.SCAUSE),
+            "stval",      self.csr.load(constants.STVAL),
+        });
+        std.debug.print("{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\t{s:>10}=[{x:>18}]\n", .{ "sscratch", self.csr.load(constants.SSCRATCH), "sip", self.csr.load(constants.SIP), "satp", self.csr.load(constants.SATP) });
     }
 
     pub fn fetch(self: *Self) !u64 {
@@ -94,6 +123,17 @@ pub const CPU = struct {
                         const data = try self.mm.load(addr, 32);
                         self.regs[rd] = data;
                         return self.pc + 4;
+                    },
+                    else => {
+                        return CPUError.InvalidInstructionError;
+                    },
+                }
+            },
+            0x0f => {
+                switch (funct3) {
+                    0x0 => {
+                        // fence
+                        return self.update_pc();
                     },
                     else => {
                         return CPUError.InvalidInstructionError;
@@ -231,6 +271,53 @@ pub const CPU = struct {
                     },
                 }
             },
+            0x2f => {
+                // RV64A: "A" standard extension for atomic instructions
+                const funct5 = (funct7 & 0b1111100) >> 2;
+                const _aq = (funct7 & 0b0000010) >> 1; // acquire access
+                const _rl = funct7 & 0b0000001; // release access
+                switch (funct3) {
+                    0x2 => {
+                        switch (funct5) {
+                            0x0 => {
+                                // AMOADD.W
+                                const t = try self.mm.load(addr, 32);
+                                try self.mm.store(self.regs[rs1], 32, t +% self.regs[rs2]);
+                                self.regs[rd] = t;
+                                return self.pc + 4;
+                            },
+                            0x1 => {
+                                //AMOSWAP.W
+                                const t = try self.mm.load(self.regs[rs1], 32);
+                                try self.mm.store(self.regs[rs1], 32, self.regs[rs2]);
+                                self.regs[rd] = t;
+                                return self.pc + 4;
+                            },
+                        }
+                    },
+                    0x3 => {
+                        switch (funct5) {
+                            0x0 => {
+                                // AMOADD.D
+                                const t = try self.mm.load(self.regs[rs1], 64);
+                                try self.mm.store(self.regs[rs1], 64, t +% self.regs[rs2]);
+                                self.regs[rd] = t;
+                                return self.pc + 4;
+                            },
+                            0x1 => {
+                                // AMOSWAP.D
+                                const t = try self.mm.load(self.regs[rs1], 64);
+                                try self.mm.store(self.regs[rs1], 64, self.regs[rs2]);
+                                self.regs[rd] = t;
+                                return self.pc + 4;
+                            },
+                        }
+                    },
+                    else => {
+                        return CPUError.InvalidInstructionError;
+                    },
+                }
+            },
             0x33 => {
                 const shamt = @intCast(u32, self.regs[rs2] & 0x1f);
                 switch (funct3) {
@@ -240,6 +327,10 @@ pub const CPU = struct {
                                 // ADD
                                 self.regs[rd] = self.regs[rs1] +% self.regs[rs2];
                                 return self.pc + 4;
+                            },
+                            0x1 => {
+                                // mul
+                                self.regs[rd] = self.regs[rs1] *% self.regs[rs2];
                             },
                             0x20 => {
                                 // SUB
@@ -340,9 +431,27 @@ pub const CPU = struct {
                                 self.regs[rd] = @intCast(u64, @intCast(i64, @intCast(i32, @intCast(u32, self.regs[rs1]) >> @intCast(u5, shamt))));
                                 return self.pc + 4;
                             },
+                            0x1 => {
+                                // divu
+                                // @todo: check for divide by 0
+                                self.regs[rd] = @intCast(u64, @intCast(u32, self.regs[rs1]) / @intCast(u32, self.regs[rs2]));
+                                return self.pc + 4;
+                            },
                             0x20 => {
-                                // SRAW
+                                // sraw
                                 self.regs[rd] = @intCast(u64, @intCast(i64, @intCast(i32, self.regs[rs1])) >> @intCast(u5, shamt));
+                                return self.pc + 4;
+                            },
+                            else => {
+                                return CPUError.InvalidInstructionError;
+                            },
+                        }
+                    },
+                    0x7 => {
+                        switch (funct7) {
+                            0x1 => {
+                                // remuw
+                                self.regs[rd] = @intCast(u64, @intCast(u32, self.regs[rs1]) % @intCast(u32, self.regs[rs2]));
                                 return self.pc + 4;
                             },
                             else => {
@@ -416,6 +525,105 @@ pub const CPU = struct {
                 const imm = @bitCast(u64, @intCast(i64, @bitCast(i32, @truncate(u32, inst & 0x80000000))) >> 11) | (inst & 0xff000) | ((inst >> 9) & 0x800) | ((inst >> 20) & 0x7fe);
                 self.regs[rd] = self.pc + 4;
                 return self.pc +% imm;
+            },
+            0x73 => {
+                const addr = @as(usize, (inst & 0xfff00000) >> 20);
+                switch (funct3) {
+                    0x0 => {
+                        switch (funct7) {
+                            0x8 => {
+                                switch (rs2) {
+                                    0x2 => {
+                                        // sret
+                                        var sstatus = self.csr.load(constants.SSTATUS);
+                                        self.mode = @intToEnum(Mode, (sstatus & constants.MASK_SPP) >> 8);
+                                        const spie = (sstatus & constants.MASK_SPIE) >> 5;
+                                        sstatus = (sstatus & ~constants.MASK_SIE) | (spie << 1);
+                                        sstatus |= constants.MASK_SPIE;
+                                        sstatus &= ~constants.MASK_SPP;
+                                        self.csr.store(constants.SSTATUS, sstatus);
+                                        return self.csr.load(constants.SEPC) & ~@intCast(u64, 0b11);
+                                    },
+                                    else => {
+                                        return CPUError.InvalidInstructionError;
+                                    },
+                                }
+                            },
+                            0x9 => {
+                                // sfence.vma
+                                return self.pc + 4;
+                            },
+                            0x18 => {
+                                switch (rs2) {
+                                    0x2 => {
+                                        // mret
+                                        var mstatus = self.csr.load(constants.MSTATUS);
+                                        self.mode = @intToEnum(Mode, (mstatus & constants.MASK_MPP) >> 11);
+                                        const mpie = (mstatus & constants.MASK_MPIE) >> 7;
+                                        mstatus = (mstatus & ~constants.MASK_MIE) | (mpie << 3);
+                                        mstatus |= constants.MASK_MPIE;
+                                        mstatus &= ~constants.MASK_MPP;
+                                        mstatus &= ~constants.MASK_MPRV;
+                                        self.csr.store(constants.MSTATUS, mstatus);
+                                        return self.csr.load(constants.MEPC) & ~@intCast(u64, 0b11);
+                                    },
+                                    else => {
+                                        return CPUError.InvalidInstructionError;
+                                    },
+                                }
+                            },
+                            else => {
+                                return CPUError.InvalidInstructionError;
+                            },
+                        }
+                    },
+                    0x1 => {
+                        // csrrw
+                        const t = self.csr.load(addr);
+                        self.csr.store(addr, self.regs[rs1]);
+                        self.regs[rd] = t;
+                        return self.pc + 4;
+                    },
+                    0x2 => {
+                        // csrrs
+                        const v = self.csr.load(addr);
+                        self.csr.store(addr, v | self.regs[rs1]);
+                        self.regs[rd] = v;
+                        return self.pc + 4;
+                    },
+                    0x3 => {
+                        // csrrc
+                        const v = self.csr.load(addr);
+                        self.csr.store(addr, v & (~self.regs[rs1]));
+                        self.regs[rd] = v;
+                        return self.pc + 4;
+                    },
+                    0x5 => {
+                        // csrrwi
+                        self.regs[rd] = self.csr.load(addr);
+                        self.csr.store(addr, rs1);
+                        return self.pc + 4;
+                    },
+                    0x6 => {
+                        // csrrsi
+                        const zimm = @intCast(u64, rs1);
+                        const v = self.csr.load(addr);
+                        self.regs[rd] = v;
+                        self.csr.store(addr, v | zimm);
+                        return self.pc + 4;
+                    },
+                    0x7 => {
+                        // csrrci
+                        const zimm = @intCast(u64, rs1);
+                        const v = self.csr.load(addr);
+                        self.regs[rd] = v;
+                        self.csr.store(addr, v & (~zimm));
+                        return self.pc + 4;
+                    },
+                    else => {
+                        return CPUError.InvalidInstructionError;
+                    },
+                }
             },
             else => {
                 return CPUError.InvalidInstructionError;
--- src/csr.zig
+++ src/csr.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const constants = @import("./constants.zig");
 
 pub const CSR = struct {
@@ -10,5 +11,23 @@ pub const CSR = struct {
         return .{
             .csrs = csrs,
         };
+    }
+
+    pub fn load(self: *Self, addr: usize) u64 {
+        switch (addr) {
+            constants.SIE => return self.csrs[constants.MIE] & self.csrs[constants.MIDELEG],
+            constants.SIP => return self.csrs[constants.MIP] & self.csrs[constants.MIDELEG],
+            constants.SSTATUS => return self.csrs[constants.MSTATUS] & constants.MASK_SSTATUS,
+            else => return self.csrs[addr],
+        }
+    }
+
+    pub fn store(self: *Self, addr: usize, value: u64) void {
+        switch (addr) {
+            constants.SIE => self.csrs[constants.MIE] = (self.csrs[constants.MIE] & (~self.csrs[constants.MIDELEG])) | (value & self.csrs[constants.MIDELEG]),
+            constants.SIP => self.csrs[constants.MIP] = (self.csrs[constants.MIE] & (~self.csrs[constants.MIDELEG])) | (value & self.csrs[constants.MIDELEG]),
+            constants.SSTATUS => self.csrs[constants.MSTATUS] = (self.csrs[constants.MSTATUS] & (~constants.MASK_SSTATUS)) | (value & constants.MASK_SSTATUS),
+            else => self.csrs[addr] = value,
+        }
     }
 };
